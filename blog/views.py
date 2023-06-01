d6ef45824f5b7d852e9b561d3167070624f25e4a
Nacho <admin@ejemplo.cl>
2023-05-31 20:47:17 -0400

    cambios en sidebar.html

--- blog/views.py
+++ blog/views.py
@@ -88,7 +88,7 @@ def agregar_producto(request):
             return redirect('producto.html')
         else:
             return render(request, 'agregar_producto.html', {'form': form})
-@login_required
+
 def producto(request):
-    productos = ProductoIngresado.objects.filter(user=request.user, datecompleted__isnull=True)
+    productos = ProductoIngresado.objects.filter()
     return render(request, 'producto.html', {'productos': productos})
